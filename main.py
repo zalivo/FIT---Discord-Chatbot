c3e55d44b5ff71a058e6cc5b1c3cd58ec942f0f2
Ivo <izaloudek5@gmail.com>
2024-02-22 10:21:44 +0100

    first commit (not running 🥱)

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import discord
+from discord import Intents, Client, Message
+from dotenv import load_dotenv
+
+# LOAD OUR TOKEN
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# BOT SETUP
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents=intents)
+
+# BOT STARTUP
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
+
+client.run(TOKEN)
+
+# MAIN
+def main() -> None:
+    client.run(TOKEN)
+
+if __name__ == '__main__':
+    main()
